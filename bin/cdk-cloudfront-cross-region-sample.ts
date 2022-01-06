8c397d9e9682300c9fbc337966f9f5dbf0fbc418
rednes <528249+rednes@users.noreply.github.com>
2022-01-06 11:39:30 +0900

    create cdk-cloudfront-cross-region-sample

--- bin/cdk-cloudfront-cross-region-sample.ts
+++ bin/cdk-cloudfront-cross-region-sample.ts
@@ -1,21 +1,42 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { CdkCloudfrontCrossRegionSampleStack } from '../lib/cdk-cloudfront-cross-region-sample-stack';
+import { AcmForCloudfrontStack } from "../lib/acm-for-cloudfront-stack";
+import { RemoteOutputStack } from "../lib/remote-output-stack";
+import { CloudfrontStack } from "../lib/cloudfront-stack";
+
+const account = '<<YOUR_AWS_ACCOUNT_NO>>';
+const domainName = '<<YOUR_PUBLIC_DOMAIN_NAME>>';
+const hostName = 'cf-cross';
+
+const envJP: cdk.Environment = {
+    account,
+    region: 'ap-northeast-1',
+};
+
+const envUS: cdk.Environment = {
+    account,
+    region: 'us-east-1',
+};
 
 const app = new cdk.App();
-new CdkCloudfrontCrossRegionSampleStack(app, 'CdkCloudfrontCrossRegionSampleStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+const acmForCloudfront = new AcmForCloudfrontStack(app, 'AcmForCloudfrontStack', {
+    env: envUS,
+    domainName,
+    hostName,
 });
+
+const remoteOutput = new RemoteOutputStack(app, 'RemoteOutput', {
+    env: envJP,
+    acm: acmForCloudfront,
+});
+
+const cloudfront = new CloudfrontStack(app, 'CloudFront', {
+    env: envJP,
+    domainName,
+    hostName,
+    acmArn: remoteOutput.acmArn,
+});
+
+cloudfront.addDependency(acmForCloudfront);
---
+++ lib/remote-output-stack.ts
@@ -0,0 +1,28 @@
+import { RemoteOutputs } from "cdk-remote-stack";
+import {
+    App,
+    Stack,
+    StackProps,
+} from 'aws-cdk-lib'
+import { AcmForCloudfrontStack } from "./acm-for-cloudfront-stack";
+
+interface RemoteOutputStackProps extends StackProps {
+    acm: AcmForCloudfrontStack;
+}
+
+export class RemoteOutputStack extends Stack {
+    public readonly acmArn: string;
+
+    constructor(scope: App, id: string, props: RemoteOutputStackProps) {
+        super(scope, id, props);
+
+        this.addDependency(props.acm)
+
+        const outputs = new RemoteOutputs(this, "Outputs", {
+            stack: props.acm,
+            alwaysUpdate: false,
+        });
+
+        this.acmArn = outputs.get("AcmArn");
+    }
+}
